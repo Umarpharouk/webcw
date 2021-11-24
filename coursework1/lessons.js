6d1f087b82a6f464e6661a22224e2a886613dff4
Umarpharouk <93410859+Umarpharouk@users.noreply.github.com>
2021-11-24 12:14:35 -0800

    Add files via upload
    commit 1

---
+++ coursework1/lessons.js
@@ -0,0 +1,100 @@
+let myArr = [
+    {
+      "id": 1001,
+      "title": "DANCE ",
+      "description": "Dance builds confidence",
+      "location":"New Lectures room",
+      "price": 20.00,
+      "image": "images/dance.jpg",
+      "availableInventory": 15
+     
+    },
+    {
+      "id": 1002,
+      "title": "SPORT CLASS",
+      "description": " All work no play, makes jack a doll boy.",
+      "location":"Alianz studio",
+      "price": 2.99,
+      "image": "images/sport.jpg",
+      "availableInventory": 16,
+    
+    },
+    {
+      "id": 1003,
+      "title": "COOKING",
+      "description": "The little masterChef.",
+      "location":"Site Campus",
+      "price": 11.00,
+      "image": "images/cooking.jpg",
+      "availableInventory": 14,
+      
+    },
+    {
+      "id": 1004,
+      "title": "SWIMMING",
+      "description": "Time to dive in water",
+      "location":"Dubai Marina",
+      "price": 7.99,
+      "image": "images/swimming.jpg",
+      "availableInventory": 13,
+     
+    },
+    {
+      "id": 1005,
+      "title": "Technology",
+      "description": "The art of self depence",
+      "location":"Sharja",
+      "price": 49.99,
+      "image": "images/tech.jpg",
+      "availableInventory": 12,
+     
+    },
+    {
+      "id": 1006,
+      "title": "MUSIC",
+      "description": "your voice is your gift.",
+      "location":"Internet City",
+      "price": 45.99,
+      "image": "images/music.jpg",
+      "availableInventory": 11,
+    },
+    {
+      "id": 1007,
+      "title": "ENGLISH ",
+      "description": "Learn to connect.",
+      "location":"Myriad",
+      "price": 49.99,
+      "image": "images/english.jpg",
+      "availableInventory": 10,
+    },
+    {
+      "id": 1008,
+      "title": "MATHS",
+      "description": "Solve with practice",
+      "location":"Bostom",
+      "price": 9.99,
+      "image": "images/maths.jpg",
+      "availableInventory": 9,
+    },
+    {
+      "id": 1009,
+      "title": " DRAWING",
+      "description": " Color your passion",
+      "location":"BUK",
+      "price": 41.09,
+      "image": "images/drawing.jpg",
+      "availableInventory": 8,
+    },
+    {
+      "id": 1010,
+      "title": "  Science CLASS",
+      "description": "Plant tree save life",
+      "location":"Room 406",
+      "price": 11.11,
+      "image": "images/science.jpg",
+      "availableInventory": 7,
+    },
+    
+
+
+  ]
